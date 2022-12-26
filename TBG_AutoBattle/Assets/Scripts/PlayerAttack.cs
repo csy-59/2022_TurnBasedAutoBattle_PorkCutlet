83dca44c4b1c54cec74d28bb68bd0211932dc9f1
csy-59 <csy010509@naver.com>
2022-12-26 16:53:29 +0900

    [Feat] 플레이어 행동 게이지 차오름 기능 완성

--- TBG_AutoBattle/Assets/Scripts/PlayerAttack.cs
+++ TBG_AutoBattle/Assets/Scripts/PlayerAttack.cs
@@ -3,17 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 행동 속도
+    [SerializeField] private float _maxAttackSpeed;
+    [SerializeField] private float _minAttackSpeed;
+    private float _attackSpeed;
+
+    private Slider _behaviourSlider;
+    private float _maxSliderValue = 1f;
+
+    private void Awake()
     {
-        
+        _behaviourSlider = GetComponent<Slider>();
+
+        SetRandomSpeed();
+
+        StartCoroutine(EAttackCool());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetRandomSpeed()
     {
-        
+        _attackSpeed = Random.Range(_minAttackSpeed, _maxAttackSpeed);
+    }
+
+    private IEnumerator EAttackCool()
+    {
+        _behaviourSlider.value = 0f;
+        float elapsedTime = 0f;
+        float currentValue = 0f;
+
+        while(true)
+        {
+            elapsedTime += Time.deltaTime * _attackSpeed;
+            currentValue = Mathf.Lerp(0, _maxSliderValue, elapsedTime);
+            _behaviourSlider.value = currentValue;
+
+            if (Mathf.Abs(_maxSliderValue - currentValue) < 0.01f)
+            {
+                _behaviourSlider.value = _maxSliderValue;
+                break;
+            }
+
+            yield return null;
+        }
+
+        Attack();
+    }
+
+
+    private void Attack()
+    {
+        Debug.Log("Attack");
+        _behaviourSlider.value = 0f;
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
     }
 }
