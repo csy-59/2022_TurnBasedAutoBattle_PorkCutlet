f803b179d6ebb6a5c631027c3e7d6e0e98dc56a8
csy-59 <csy010509@naver.com>
2022-12-29 11:46:45 +0900

    [Fix] 강타 스킬 EffectOtherPlayerSkill를 받아 사용할 수 있도록 수정

--- TBG_AutoBattle/Assets/Scripts/Player/Skills/StrikeAttackSkill.cs
+++ TBG_AutoBattle/Assets/Scripts/Player/Skills/StrikeAttackSkill.cs
@@ -2,51 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StrikeAttackSkill : PlayerSkill
+public class StrikeAttackSkill : EffectOtherPlayerSkill
 {
-    // 플레이어
-    [SerializeField] private Collider _playerCollider;
-    [SerializeField] private Collider _stickCollider;
-    [SerializeField] private PlayerMovement _playerMovement;
-
-    [SerializeField] private Transform _targetPosition;
-    private Vector3 _originalPosition;
-
     [SerializeField] private float _damagePercentage = 1.3f;
 
     private void Awake()
     {
         Init();
-        _originalPosition = transform.position;
     }
 
-    protected override void UseSkill()
+    protected override void EffectOtherPlayer (GameObject otherPlayer)
     {
-        Attack();
-    }
+        PlayerHealth _otherHealth = _playerMovement.OtherPlayer.GetComponent<PlayerHealth>();
+        Debug.Assert(_otherHealth);
 
-    private void Attack()
-    {
-        PrepareForAttack();
-        _playerMovement.MoveToPosition(_targetPosition.position, () =>
-        {
-            PlayerHealth _otherHealth = _playerMovement.OtherPlayer.GetComponent<PlayerHealth>();
-            Debug.Assert(_otherHealth);
-
-            _otherHealth.TakeDamage(Mathf.RoundToInt(_playerAct.Damage * _damagePercentage));
-
-            _playerMovement.MoveToPosition(_originalPosition, () =>
-            {
-                _stickCollider.enabled = false;
-                _playerCollider.enabled = true;
-                OnUseSkill.Invoke(false);
-            });
-        });
-    }
-
-    private void PrepareForAttack()
-    {
-        _playerCollider.enabled = false;
-        _stickCollider.enabled = true;
+        _otherHealth.TakeDamage(Mathf.RoundToInt(_playerAct.Damage * _damagePercentage));
     }
 }
